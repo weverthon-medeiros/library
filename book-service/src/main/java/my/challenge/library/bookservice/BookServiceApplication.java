8f28cd64d131f8014fb9eafea561f084be45bebe
Weverthon Medeiros <weverthon@gmail.com>
2019-05-07 23:27:00 +0100

    Added ModelMapper

--- book-service/src/main/java/my/challenge/library/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/my/challenge/library/bookservice/BookServiceApplication.java
@@ -1,10 +1,17 @@
 package my.challenge.library.bookservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookServiceApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
