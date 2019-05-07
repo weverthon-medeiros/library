bed00adb7defa81825051532725369f09c8c68f1
Weverthon Medeiros <weverthon@gmail.com>
2019-05-07 22:32:59 +0100

    Added book-service structure

---
+++ book-service/src/main/java/my/challenge/library/bookservice/BookServiceApplication.java
@@ -0,0 +1,13 @@
+package my.challenge.library.bookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+}
